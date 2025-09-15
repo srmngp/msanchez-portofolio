eaf3308f1561e846e85171b4cd87572cc8726971
srmngp <sr.mngp@gmail.com>
2025-09-15 19:29:44 +0200

    remove unsed fonts

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./header";
 import Footer from "./footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -22,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
       <div className="min-h-screen bg-white text-gray-900">
         <div className="mx-4 md:mx-12">
